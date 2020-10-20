6a7c7b7406aff147bd881b53afa0f1a12fcc0ffc
janQuabu <jan.rodriguez@quabu.eu>
2020-10-20 14:36:13 +0200

    waveform without class. leaving label encoding (-1, 0, 1) because it's better one-hot encoding

--- read_datasets.py
+++ read_datasets.py
@@ -9,46 +9,17 @@ adult_data, adult_meta = arff.loadarff('./datasets/adult.arff')
 waveform_data, waveform_meta = arff.loadarff('./datasets/waveform.arff')
 cn4_data, cn4_meta = arff.loadarff('./datasets/connect-4.arff')
 
-adult_data_tmp = np.empty_like(adult_data)
-for idx,row in enumerate(adult_data[:10]):
-    row_tmp = ()
-    for attr in row:
-        try:
-            decoded = attr.decode()
-            row_tmp += (decoded,)
-            print(row_tmp)
-        except AttributeError:
-            row_tmp += (attr,)
-            print(row_tmp)
-    adult_data_tmp[idx] = row_tmp
-    print(adult_data_tmp[:10])
+adult_df = pd.DataFrame(adult_data)
+adult_df = adult_df.drop("class",axis=1)
+adult_df = pd.get_dummies(adult_df)
 
 
-
-adult_data_pd = pd.DataFrame(adult_data_tmp)
-ohe = preprocessing.OneHotEncoder()
-
-adult_data_pd = ohe.fit_transform(adult_data_pd)
-zoo_p_data = np.zeros([len(zoo_data), len(zoo_data[0])])
-
-for i in range(len(zoo_data)):
-    for j in range(1,13):
-         zoo_p_data[i,j] = zoo_encoder.transform([zoo_data[i][j]])[0]
-    # We have to scale this range to [0,1] but I don't understand the method xd
-    zoo_p_data[i, 13] = zoo_data[i][13]
-    for j in range(14, 17):
-         zoo_p_data[i,j] = zoo_encoder.transform([zoo_data[i][j]])[0]
-
-zoo_final_data = zoo_p_data[:, 1:-1]
-
-waveform_final_data = np.zeros([len(waveform_data), len(waveform_data[0])-1])
-
-for i in range(len(waveform_data)):
-    for j in range(len(waveform_data[0])-1):
-        waveform_final_data[i,j] = waveform_data[i][j]
+waveform_df = pd.DataFrame(waveform_data)
+waveform_df = waveform_df.drop("class",axis=1)
 
 cn4_encoder = preprocessing.LabelEncoder()
-cn4_encoder.fit([b'x', b'b', b'o'])
+cn4_encoder.fit_transform([b'x', b'b', b'o'])
 
 cn4_proc_data = []
 [cn4_proc_data.append(cn4_encoder.transform(x.tolist()[0:-1])) for x in cn4_data]
+cn4_proc_data = [[cell-1 for cell in row] for row in cn4_proc_data]
