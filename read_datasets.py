93ff1eae007b36c08b42134d65c6149460d739a3
janQuabu <jan.rodriguez@quabu.eu>
2020-10-20 18:27:15 +0200

    Normalization of the data and comments added. Missing values handling remaining

--- read_datasets.py
+++ read_datasets.py
@@ -10,13 +10,27 @@ waveform_data, waveform_meta = arff.loadarff('./datasets/waveform.arff')
 cn4_data, cn4_meta = arff.loadarff('./datasets/connect-4.arff')
 
 adult_df = pd.DataFrame(adult_data)
+# Drop 'class' attribute, as it is the real label
 adult_df = adult_df.drop("class",axis=1)
+# One-hot encoding the categorical attributes
 adult_df = pd.get_dummies(adult_df)
+# Normalize the data
+scaler = preprocessing.MinMaxScaler()
+adult_df_scaled = scaler.fit_transform(adult_df.values)
+adult_df = pd.DataFrame(adult_df_scaled)
 
 
 waveform_df = pd.DataFrame(waveform_data)
+# Drop 'class' attribute, as it is the real label
 waveform_df = waveform_df.drop("class",axis=1)
+# Normalize the data
+waveform_df_scaled = scaler.fit_transform(waveform_df.values)
+waveform_df = pd.DataFrame(waveform_df_scaled)
+
 
 cn4_df = pd.DataFrame(cn4_data)
+# Drop 'class' attribute, as it is the real label
 cn4_df = cn4_df.drop("class",axis=1)
+# One-hot encoding the categorical attributes
 cn4_df = pd.get_dummies(cn4_df)
+# We do not need to normalize the data since all values are zeroes and ones
