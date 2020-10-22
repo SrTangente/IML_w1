4d8c3e97408a5640c0839d6461929203943532d6
janQuabu <jan.rodriguez@quabu.eu>
2020-10-22 15:11:42 +0200

    Replace missing values on adult dataset (waveform and cn4 don't have missing values)

--- read_datasets.py
+++ read_datasets.py
@@ -12,6 +12,8 @@ cn4_data, cn4_meta = arff.loadarff('./datasets/connect-4.arff')
 adult_df = pd.DataFrame(adult_data)
 # Drop 'class' attribute, as it is the real label
 adult_df = adult_df.drop("class",axis=1)
+# Replace missing values
+adult_df = adult_df.replace(b"?",np.nan)
 # One-hot encoding the categorical attributes
 adult_df = pd.get_dummies(adult_df)
 # Normalize the data
@@ -23,6 +25,7 @@ adult_df = pd.DataFrame(adult_df_scaled)
 waveform_df = pd.DataFrame(waveform_data)
 # Drop 'class' attribute, as it is the real label
 waveform_df = waveform_df.drop("class",axis=1)
+# No missing values, no step needed
 # Normalize the data
 waveform_df_scaled = scaler.fit_transform(waveform_df.values)
 waveform_df = pd.DataFrame(waveform_df_scaled)
@@ -31,6 +34,7 @@ waveform_df = pd.DataFrame(waveform_df_scaled)
 cn4_df = pd.DataFrame(cn4_data)
 # Drop 'class' attribute, as it is the real label
 cn4_df = cn4_df.drop("class",axis=1)
+# No missing values, no step needed
 # One-hot encoding the categorical attributes
 cn4_df = pd.get_dummies(cn4_df)
 # We do not need to normalize the data since all values are zeroes and ones
