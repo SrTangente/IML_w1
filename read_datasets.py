cc58e73e11f541d6e1d6983a6b54bcdd963fff2b
janQuabu <jan.rodriguez@quabu.eu>
2020-10-26 22:37:25 +0100

    Encode classes as numpy float array

--- read_datasets.py
+++ read_datasets.py
@@ -1,15 +1,14 @@
 from scipy.io import arff
-from sklearn.cluster import DBSCAN
 import numpy as np
 import pandas as pd
 from sklearn import preprocessing
-from CompareDBSCAN import compareDBSCAN_alg, compareDBSCAN_metric
+from sklearn.preprocessing import LabelEncoder
 
 
 def read_waveform():
     waveform_data, waveform_meta = arff.loadarff('./datasets/waveform.arff')
     waveform_df = pd.DataFrame(waveform_data)
-    # Drop 'class' attribute, as it is the real label
+    # Drop 'class' attribute from the training, as we are using unsupervised learning
     classes = waveform_df["class"]
     waveform_df = waveform_df.drop("class", axis=1)
     # No missing values, no step needed
@@ -17,7 +16,7 @@ def read_waveform():
     scaler = preprocessing.MinMaxScaler()
     waveform_df_scaled = scaler.fit_transform(waveform_df.values)
     waveform_df = pd.DataFrame(waveform_df_scaled)
-    return waveform_df.to_numpy(), classes
+    return waveform_df.to_numpy(), classes.to_numpy(dtype=np.float)
 
 
 def read_adult():
@@ -29,24 +28,30 @@ def read_adult():
     # The index can be reset after dropna() by calling reset_index(drop=True)
     # The index reset is omitted for now in order not to lose track of the missing values dropped
     adult_df = adult_df.dropna()
-    # Drop 'class' attribute, as it is the real label
+    # Drop 'class' attribute from the training, as we are using unsupervised learning
     classes = adult_df["class"]
     adult_df = adult_df.drop("class", axis=1)
+    # Encode classes [b'<=50K', b'>50K'] to numbers
+    enc = LabelEncoder()
+    classes = enc.fit_transform(classes).astype(float)
     # One-hot encoding the categorical attributes
     adult_df = pd.get_dummies(adult_df)
     # Normalize the data
     scaler = preprocessing.MinMaxScaler()
     adult_df_scaled = scaler.fit_transform(adult_df.values)
     adult_df = pd.DataFrame(adult_df_scaled)
-    return adult_df.to_numpy(), classes
+    return adult_df.to_numpy(), classes.to_numpy(dtype=np.float)
 
 
 def read_cn4():
     cn4_data, cn4_meta = arff.loadarff('./datasets/connect-4.arff')
     cn4_df = pd.DataFrame(cn4_data)
-    # Drop 'class' attribute, as it is the real label
+    # Drop 'class' attribute from the training, as we are using unsupervised learning
     classes = cn4_df["class"]
     cn4_df = cn4_df.drop("class", axis=1)
+    # Encode classes [b'draw', b'loss', b'win'] to numbers
+    enc = LabelEncoder()
+    classes = enc.fit_transform(classes).astype(float)
     # No missing values, no step needed
     # One-hot encoding the categorical attributes
     cn4_df = pd.get_dummies(cn4_df)
