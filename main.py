d6f78ca7699c19899433109b2a0b6419279f0668
janQuabu <jan.rodriguez@quabu.eu>
2020-11-16 21:47:58 +0100

    Main adapted to work 2

--- main.py
+++ main.py
@@ -12,7 +12,7 @@ from visualize_km_pca_tsne import visualize_km
 
 
 def input_dataset():
-    print("Select dataset to which execute the unsupervised algorithms:")
+    print("Select dataset to which execute and visualize K-Means:")
     print("1 adult")
     print("2 waveform")
     print("3 vowel")
@@ -31,24 +31,24 @@ def input_dataset():
     return num_dataset
 
 
-def input_algorithm():
+def input_visualization():
     print("------------------------")
-    print("Select algorithm to use:")
+    print("Select visualization:")
     print("1 Compare PCA effects on k-means")
     print("2 Visualize PCA vs t-SNE")
-    num_algorithm = 0
+    num_visualization = 0
     while True:
         try:
-            num_algorithm = int(input("Algorithm number: "))
+            num_visualization = int(input("Visualization number: "))
         except:
             print("Incorrect value. It has to be one of (1, 2).")
             continue
-        if num_algorithm in range(1, 2):
+        if num_visualization in range(1, 3):
             break
         else:
             print("Incorrect value. It has to be one of (1, 2).")
 
-    return num_algorithm
+    return num_visualization
 
 
 def read_dataset(num_dataset):
@@ -57,20 +57,20 @@ def read_dataset(num_dataset):
     elif num_dataset == 2:
         return read_waveform(), 3
     else:
-        return read_vowel(), 3
+        return read_vowel(), 11
 
 
-def execute_algorithm(num_dataset, num_algorithm):
+def execute_visualization(num_dataset, num_visualization):
     """
-    Execute the selected algorithm on the selected dataset with the best parameters obtained
+    Execute the selected visualization on the selected dataset with the best parameters obtained
     :param num_dataset: the number of the dataset selected
-    :param num_algorithm: the number of the dataset selected
+    :param num_visualization: the number of the dataset selected
     :return: the tagged_data predicted and the real classes of the dataset
     """
     (data, classes), k = read_dataset(num_dataset)
-    print("Executing algorithm...")
+    print("Executing visualization...")
     print("This process can take some minutes")
-    if num_algorithm == 1:
+    if num_visualization == 1:
         if num_dataset < 3:
             k_values = [2, 3, 4]
             p_values = [0, 20, 10, 5]
@@ -78,13 +78,11 @@ def execute_algorithm(num_dataset, num_algorithm):
             k_values = [7, 11, 15]
             p_values = [0, 6, 4, 2]
         evaluate_scatter(data, classes, kmeans, k_values, p_values)
-    elif num_algorithm == 2:
-        visualize_km(data, classes)
-
-    return tagged_data, classes
+    elif num_visualization == 2:
+        visualize_km(data, classes, k)
 
 
 if __name__ == "__main__":
     num_dataset = input_dataset()
-    num_algorithm = input_algorithm()
-    tagged_data, classes = execute_algorithm(num_dataset, num_algorithm)
+    num_visualization = input_visualization()
+    execute_visualization(num_dataset, num_visualization)
--- visualize_km_pca_tsne.py
+++ visualize_km_pca_tsne.py
@@ -3,30 +3,22 @@ import read_datasets
 import evaluate
 from sklearn import decomposition, manifold
 from matplotlib import pyplot as plt
+from pca import pca
 
 
-#data, classes = read_datasets.read_vowel()
-#data, classes = read_datasets.read_waveform()
-data, classes = read_datasets.read_adult()
 
-def visualize_km(data, classes):
-    k = 2
-    num_components = 2
-    perplexity = 30
-    alpha = 0.5
+def visualize_km(data, classes, k=3, num_components=5, perplexity=30, alpha=0.5):
+
 
     # Compute k-means without dimensionality reduction
     labels_without = kmeans(data, k)[:, -1]
 
     # Compute k-means with PCA
-    pca = decomposition.PCA(num_components)
-    pca.fit(data)
-    print('Variances: ', pca.explained_variance_)
-    pca_data = pca.transform(data)
+    pca_data = pca(data, False)[:, :num_components]
     labels_pca = kmeans(pca_data, k)[:, -1]
 
     # Project data to t-SNE
-    tsne = manifold.TSNE(num_components, perplexity=perplexity)
+    tsne = manifold.TSNE(2, perplexity=perplexity)
     tsne_data = tsne.fit_transform(data)
 
     fig1, axes1 = plt.subplots(3, 2, figsize=(8, 8))
@@ -35,38 +27,36 @@ def visualize_km(data, classes):
     axes1[0, 0].set_title('(a) Original labels (PCA)')
     axes1[0, 0].set_xlabel('1st principal component')
     axes1[0, 0].set_ylabel('2nd principal component')
-    axes1[0, 0].scatter(pca_data[:, 0], pca_data[:, 1], c=classes, alpha=alpha)
+    axes1[0, 0].scatter(pca_data[:, 0], pca_data[:, 1], c=classes, alpha=alpha, cmap="rainbow")
 
     # Plot original labels with 2 principal components (t-SNE)
     axes1[0, 1].set_title('(b) Original labels (t-SNE)')
     axes1[0, 1].set_xlabel('1st principal component')
     axes1[0, 1].set_ylabel('2nd principal component')
-    axes1[0, 1].scatter(tsne_data[:, 0], tsne_data[:, 1], c=classes, alpha=alpha)
+    axes1[0, 1].scatter(tsne_data[:, 0], tsne_data[:, 1], c=classes, alpha=alpha, cmap="rainbow")
 
     # Plot k-means without dim red (PCA)
     axes1[1, 0].set_title('(c) K-Means with original data (PCA)')
     axes1[1, 0].set_xlabel('1st principal component')
     axes1[1, 0].set_ylabel('2nd principal component')
-    axes1[1, 0].scatter(pca_data[:, 0], pca_data[:, 1], c=labels_without, alpha=alpha)
+    axes1[1, 0].scatter(pca_data[:, 0], pca_data[:, 1], c=labels_without, alpha=alpha, cmap="rainbow")
 
     # Plot k-means without dim red (t-SNE)
     axes1[1, 1].set_title('(d) K-Means with original data (t-SNE)')
     axes1[1, 1].set_xlabel('1st principal component')
     axes1[1, 1].set_ylabel('2nd principal component')
-    axes1[1, 1].scatter(tsne_data[:, 0], tsne_data[:, 1], c=labels_without, alpha=alpha)
+    axes1[1, 1].scatter(tsne_data[:, 0], tsne_data[:, 1], c=labels_without, alpha=alpha, cmap="rainbow")
 
     # Plot k-means after PCA (PCA)
     axes1[2, 0].set_title('(e) K-Means after PCA (PCA)')
     axes1[2, 0].set_xlabel('1st principal component')
     axes1[2, 0].set_ylabel('2nd principal component')
-    axes1[2, 0].scatter(pca_data[:, 0], pca_data[:, 1], c=labels_pca, alpha=alpha)
+    axes1[2, 0].scatter(pca_data[:, 0], pca_data[:, 1], c=labels_pca, alpha=alpha, cmap="rainbow")
 
     # Plot k-means after PCA (t-SNE)
     axes1[2, 1].set_title('(f) K-Means after PCA (t-SNE)')
     axes1[2, 1].set_xlabel('1st principal component')
     axes1[2, 1].set_ylabel('2nd principal component')
-    axes1[2, 1].scatter(tsne_data[:, 0], tsne_data[:, 1], c=labels_pca, alpha=alpha)
+    axes1[2, 1].scatter(tsne_data[:, 0], tsne_data[:, 1], c=labels_pca, alpha=alpha, cmap="rainbow")
 
     plt.show()
-
-visualize_km(data, classes)
